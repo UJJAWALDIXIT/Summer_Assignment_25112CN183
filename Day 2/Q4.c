62f308364eb40b678e26017d109f961dc92cb9cb
Ujjawal Dixit <ujjwaldixit9452@gmail.com>
2026-06-02 21:57:41 +0530

    Added Day2 Q4 palindrome number program

---
+++ Day 2/Q4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int num, original, reverse = 0, rem;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    original = num;
+
+    while (num >w4
+         0) {
+        rem = num % 10;
+        reverse = reverse * 10 + rem;
+        num = num / 10;
+    }
+
+    if (original == reverse)
+        printf("Palindrome Number\n");
+    else
+        printf("Not a Palindrome Number\n");
+
+    return 0;
+}
